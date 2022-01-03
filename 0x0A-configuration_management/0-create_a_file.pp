a2a9b0db6c73ee19ad1caf11854eed59efce9771
122-63 <3469@holbertonschool.com>
2022-01-03 15:49:24 -0500

    update style

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file
 
 file {'/tmp/school':
-ensure   => 'file',
-mode     => '0744',
-owner    => 'www-data',
-group    => 'www-data',
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # create a manifest that kills a process named killmenow
 
 exec { 'kill':
-command => 'pkill -f killmenow',
+command  => 'pkill -f killmenow',
 provider => shell,
 }
